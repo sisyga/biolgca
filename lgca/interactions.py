8b5a3b0afb8a5af3be62aea0997004429aedeaa6
sisyga <simon.syga@web.de>
2019-05-24 18:00:36 +0200

    work on wetting interaction f. pf project

--- lgca/interactions.py
+++ lgca/interactions.py
@@ -200,11 +200,19 @@ def wetting(lgca):
     :param lgca:
     :return:
     """
+    k_rest = (lgca.n_crit + 1) / (lgca.velocitychannels + 1)
+    rho = lgca.cell_density[..., None] / lgca.K
+    birth = npr.random(lgca.nodes.shape) < lgca.r_b * rho / (1 - rho)
+    nbs = lgca.nb_sum(lgca.cell_density) + lgca.cell_density - 1
+    nbs /= lgca.n_crit
+    death = npr.random(lgca.nodes.shape) < lgca.r_b * nbs[..., None]
+    ds = (1 - lgca.nodes) * birth - lgca.nodes * death
+    np.add(lgca.nodes, ds, out=lgca.nodes, casting='unsafe')
+    lgca.update_dynamic_fields()
     newnodes = lgca.nodes.copy()
     relevant = (lgca.cell_density[lgca.nonborder] > 0) & \
                (lgca.cell_density[lgca.nonborder] < lgca.K)
     coords = [a[relevant] for a in lgca.nonborder]
-
 
     # subs_dens = lgca.K - lgca.cell_density * 1.
     subs_dens = 1 / (lgca.cell_density + 1)
@@ -212,14 +220,17 @@ def wetting(lgca):
                   np.divide(1., lgca.cell_density[..., None], where=lgca.cell_density[..., None] > 0,
                             out=np.zeros_like(lgca.cell_density[..., None], dtype=float))
     # g_subs = lgca.gradient(subs_dens)
-    nbs = np.clip(lgca.nb_sum(lgca.cell_density) + lgca.cell_density, a_min=None, a_max=lgca.n_crit)
-
-    adh_weight = lgca.channel_weight(nbs) - nbs[..., None] - 1
+    # nbs = np.clip(lgca.nb_sum(lgca.cell_density) + lgca.cell_density, a_min=None, a_max=lgca.n_crit)
+    nbs = lgca.nb_sum(lgca.cell_density) + lgca.cell_density
+    nbs *= (1 - nbs / lgca.n_crit / 2) * np.heaviside(1 - nbs / lgca.n_crit / 2, 0) / lgca.n_crit
+    adh_weight = lgca.channel_weight(nbs) - nbs[..., None]
     resting = lgca.nodes[..., lgca.velocitychannels:].sum(-1)
     resting = lgca.nb_sum(resting)
+    #resting = np.clip(resting, a_min=0, a_max=lgca.n_crit)
     # resting = np.clip(resting, a_min=None, a_max=lgca.n_crit)
     g = lgca.calc_flux(lgca.nodes)
     g = lgca.nb_sum(g)
+    #g = np.clip(g, a_max=lgca.n_crit, a_min=-lgca.n_crit)
     # g = np.divide(g, lgca.cell_density[..., None], where=lgca.cell_density[..., None]>0, out=np.zeros_like(g))
     # g = np.divide(g, nbs[..., None], where=nbs[..., None]>0, out=np.zeros_like(g))
 
@@ -228,17 +239,20 @@ def wetting(lgca):
         n = lgca.cell_density[coord]
         permutations = lgca.permutations[n]
         # velocityc = permutations[:, :lgca.velocitychannels]
-        restc = permutations[:, lgca.velocitychannels:]
+        restc = permutations[:, lgca.velocitychannels:].sum(-1)
         j = lgca.j[n]
         j_nb = g[coord]
         weights = np.exp(  # -lgca.beta * nbs[coord] * np.linalg.norm(j - j_nb[:, None], axis=0) / lgca.n_crit
-            lgca.beta / lgca.velocitychannels / 4 * (
-                        j_nb[0] * j[0] + j_nb[1] * j[1] + resting[coord] * restc.sum(-1) * 4 / lgca.restchannels ** 2)
-            # + lgca.gamma * np.einsum('i,ij', g[coord], j) + lgca.gamma * resting[coord] * restc.sum(-1)
-            + lgca.beta * np.dot(permutations[:, :lgca.velocitychannels], adh_weight[coord]) / lgca.n_crit
+            lgca.beta * (j_nb[0] * j[0] + j_nb[1] * j[1]) / lgca.velocitychannels / 2
+            + lgca.beta * resting[coord] * restc * (1 - restc / k_rest / 2) * 2 / lgca.velocitychannels / k_rest
+            + lgca.beta * np.dot(permutations[:, :lgca.velocitychannels],
+                                 adh_weight[coord])  # * np.heaviside(1 - nbs[coord] / lgca.n_crit, 0)
             # + lgca.alpha * np.einsum('i,ij', g_subs[coord], j)
             + lgca.alpha * np.dot(permutations[:, :lgca.velocitychannels], subs_weight[coord])
         ).cumsum()
+        # print('Alignment:', (j_nb[0] * j[0] + j_nb[1] * j[1]) / 4 / lgca.velocitychannels)
+        # print('Resting:', resting[coord] * restc.sum(-1) / lgca.restchannels ** 2 / lgca.velocitychannels)
+        #print('Adhesion:', np.dot(permutations[:, :lgca.velocitychannels], adh_weight[coord]) / lgca.n_crit)
         ind = bisect_left(weights, random() * weights[-1])
         newnodes[coord] = permutations[ind]
 
