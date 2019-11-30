91732564f80ab1bc8321507834d02929f8396166
sisyga <simon.syga@web.de>
2019-11-30 13:00:51 +0100

    successful testing

--- testing.py
+++ testing.py
@@ -5,10 +5,10 @@ import matplotlib.pyplot as plt
 
 nodes = np.zeros((50, 50, 12), dtype=bool)
 nodes[25, 25, -6:] = 1
-lgca = get_lgca(interaction='alignment', beta=4, restchannels=6, density=0.02)#, nodes=nodes)
+lgca = get_lgca(interaction='alignment', beta=4, restchannels=6, density=0.01)#, nodes=nodes)
 lgca.interaction = leup_test
 lgca.r_b = 0.1
-lgca.r_d = 0.01
+lgca.r_d = 0.05
 ani = lgca.live_animate_density()
 plt.show()
 lgca.timeevo(5, record=True)
