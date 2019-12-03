931c6efe1e27b5c4e7b528429f64de69b8245638
Franzi2729 <_>
2019-12-03 15:57:38 +0100

    add offspring_script

---
+++ offspring_script.py
@@ -0,0 +1,38 @@
+from lgca import get_lgca
+from lgca.helpers import *
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+from os import environ as env
+
+dim = int(env['DIMS'])
+rc = int(env['RESTCHANNELS'])
+rep = int(env['REPETITIONS'])
+
+dens = 1
+birthrate = 0.5
+deathrate = 0.02
+
+
+saving_data = True
+
+for i in range(0, rep):
+    start = time.time()
+    name = str(2*dim + dim*rc) + str(dim)
+
+    lgca= get_lgca(ib=True, geometry='lin', interaction='inheritance', bc='reflecting',\
+           density = dens, dims = dim, r_b = birthrate, variation = False, restchannels = rc ,r_d = deathrate)
+    id = name + '_'+ str(i)
+    lgca.timeevo_until_hom(record=True)
+    offsprings = np.zeros((len(lgca.props_t), len(lgca.props_t[0]['num_off'])))
+    for t in range(len(lgca.props_t)):
+        for anc in range(len(lgca.props_t[0]['num_off'])):
+            offsprings[t, anc] = lgca.props_t[t]['num_off'][anc]
+    if saving_data:
+        np.save('saved_data/' + str(id) + '_props_t', lgca.props_t)
+        np.save('saved_data/' + str(id) + '_offsprings', offsprings)
+        np.save('saved_data/' + str(id) + '_nodes_t', lgca.nodes_t)
+        np.savez('saved_data/' + str(id) + '_Parameter', density = lgca.density, restchannels = lgca.restchannels,\
+        dimension=lgca.l, kappa=lgca.K, rb=lgca.r_b, rd=lgca.r_d, m=lgca.r_int)
+    ende = time.time()
+    print('{:5.3f}s'.format(ende-start))
