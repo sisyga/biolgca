changes to nove: - fixed calculation of entropy, normalised entropy, polar alignment parameter and mean alignment, generalised to any dimensions and moved to nove base class - removed duplicate code for alignment interactions: made center inclusion an LGCA attribute - removed attribute eff_dens and override of update_dynamic_fields (unnecessary) - fixed capacity handling in 1d set_dims for given nodes - added automatic slicing of self.density_t (if not passed) in x and t for 1D density plotting

lgca/lgca_1d.py
@@ -268,6 +268,10 @@ class LGCA_noVE_1D(LGCA_1D, LGCA_noVE_base):
                 raise Exception('Only one resting channel allowed, \
                  but {} resting channels specified!'.format(self.K - self.velocitychannels))
             self.dims = self.l,
+            if capacity is not None:
+                self.capacity = capacity
+            else:
+                self.capacity = self.K
             return
         # default value for dimension
         elif dims is None:
@@ -326,6 +330,10 @@ class LGCA_noVE_1D(LGCA_1D, LGCA_noVE_base):
         # set values for unused arguments
         if density_t is None:
             density_t = self.dens_t
+            if offset_x is not None and isinstance(offset_x, int):
+                density_t = density_t[:, offset_x:]
+            if offset_t is not None and isinstance(offset_t, int):
+                density_t = density_t[offset_t:, :]
         if figsize is None:
             figsize = estimate_figsize(density_t.T, cbar=True)
 
@@ -421,65 +429,7 @@ class LGCA_noVE_1D(LGCA_1D, LGCA_noVE_base):
          # add central value
          if addCenter:
             sum += qty
-         # used for summing up fluxes
          return sum
-
-    def calc_entropy(self):
-        """
-        Calculate entropy of the lattice.
-        :return: entropy according to information theory as scalar
-        """
-        # calculate relative frequencies
-        rel_freq = self.nodes[self.nonborder].sum(-1)/self.nodes[self.nonborder].sum()
-        # empty lattice sites are not considered in the sum
-        a = np.where(rel_freq > 0, np.log(rel_freq), 0)
-        return -np.multiply(rel_freq, a).sum()
-
-    def calc_normalized_entropy(self):
-        """
-        Calculate entropy of the lattice normalized to maximal possible entropy.
-        :return: normalized entropy as scalar
-        """
-        smax = np.log(self.l)
-        self.smax = smax
-        return 1 - self.calc_entropy()/smax
-
-    def calc_polar_alignment_parameter(self):
-        """
-        Calculate the polar alignment parameter.
-        The polar alignment parameter is a measure for global agreement of particle orientation in the lattice.
-        It is calculated as the magnitude of the sum of the velocities of all particles normalized by the number of particles.
-        :return: polar alignment parameter as scalar
-        """
-        return np.abs(self.calc_flux(self.nodes)[self.nonborder].sum()/self.nodes[self.nonborder].sum())
-
-    def calc_mean_alignment(self):
-        """
-        Calculate the mean alignment measure.
-        The mean alignment is a measure for local alignment of particle orientation in the lattice.
-        It is calculated as the agreement in direction between the ﬂux of a lattice site and the ﬂux of the director ﬁeld
-        summed up and normalized over all lattice sites.
-        :return: mean alignment as scalar
-        """
-        # retrieve particle numbers and fluxes from instance
-        no_neighbors = self.nb_sum(np.ones(self.cell_density[self.nonborder].shape))
-        f = self.calc_flux(self.nodes[self.nonborder])
-        d = self.cell_density[self.nonborder]
-        d_div = np.where(d > 0, d, 1)
-        # calculate flux director field and normalize by number of neighbors
-        f_norm = f.flatten()/d_div
-        f_norm = self.nb_sum(f_norm)
-        f_norm = f_norm/no_neighbors
-        # calculate agreement between flux and director field flux, take mean over lattice
-        return (np.dot(f_norm, f)).sum() / d.sum() #first sum probably unnecessary
-
-
-    def update_dynamic_fields(self):
-        """
-        Update "fields" that store important variables to compute other dynamic steps.
-        """
-        self.cell_density = self.nodes.sum(-1)
-        self.eff_dens = self.nodes[self.nonborder].sum()/(self.capacity * self.l)
 
 
 if __name__ == '__main__':

lgca/nove_interactions.py
@@ -1,16 +1,14 @@
 import numpy as np
 import numpy.random as npr
-
+import matplotlib.pyplot as plt
 try:
     from .interactions import tanh_switch
 except ImportError:
     from interactions import tanh_switch
 
-
 def dd_alignment(lgca):
     """
-    Rearrangement step for density-dependent alignment interaction excluding the central lattice site.
-	This interaction only works in 1D so far!
+    Rearrangement step for density-dependent alignment interaction including the central lattice site.
     """
     newnodes = lgca.nodes.copy()
     # filter for nodes that are not virtual border lattice sites
@@ -18,7 +16,8 @@ def dd_alignment(lgca):
     coords = [a[relevant] for a in lgca.nonborder]
     # calculate director field
     g = lgca.calc_flux(lgca.nodes)  # flux for each lattice site
-    g = lgca.nb_sum(g)  # sum of flux of neighbors for each lattice site
+    g = lgca.nb_sum(g, addCenter=lgca.nb_include_center)  # sum of flux of neighbors for each lattice site
+
 
     # loop through lattice sites and reassign particle directions
     for coord in zip(*coords):
@@ -26,28 +25,28 @@ def dd_alignment(lgca):
         n = lgca.cell_density[coord]
         # calculate transition probabilities for directions
         weights = np.exp(lgca.beta * np.einsum('i,ij', g[coord], lgca.c))
+
         z = weights.sum()
-        weights = (weights / z)
-        #print("Coord:")
-        #print(coord)
-        #print("Weights:")
-        #print(weights)
+        # to prevent divisions by zero if the weight is zero
+        aux = np.nan_to_num(z)
+        weights = np.nan_to_num(weights)
+        weights = (weights / aux)
+        # In case there are some rounding problems
+        if weights.sum() > 1:
+            weights = (weights / weights.sum())
 
         # reassign particle directions
-        #sample = npr.choice(lgca.c[0], size=(n,), replace=True, p=weights)
+        sample = npr.multinomial(n, weights,)
 
-        sample = npr.multinomial(lgca.c[0,0], weights, size = (n,) )
+        newnodes[coord] = sample
 
-        print("Sample:")
-        print(sample)
-        newnodes[coord] = np.array([np.count_nonzero(sample == 1), np.count_nonzero(sample == -1)])
     lgca.nodes = newnodes
+
 
 
 def di_alignment(lgca):
     """
-    Rearrangement step for density-independent alignment interaction excluding the central lattice site.
-	This interaction only works in 1D so far!
+    Rearrangement step for density-independent alignment interaction including the central lattice site.
     """
     newnodes = lgca.nodes.copy()
     # filter for nodes that are not virtual border lattice sites
@@ -55,11 +54,12 @@ def di_alignment(lgca):
     coords = [a[relevant] for a in lgca.nonborder]
     # calculate director field
     g = lgca.calc_flux(lgca.nodes)  # flux for each lattice site
-    g = lgca.nb_sum(g)  # sum of flux of neighbors for each lattice site
+    g = lgca.nb_sum(g, addCenter=lgca.nb_include_center)  # sum of flux of neighbors for each lattice site
+
     # normalize director field by number of neighbors
-    nsum = lgca.nb_sum(lgca.cell_density)[None, ...]
-    np.maximum(nsum, 1, out=nsum) # avoid dividing by zero later
-    g = g/ nsum.T
+    nsum = lgca.nb_sum(lgca.cell_density, addCenter=lgca.nb_include_center)[None, ...]
+    np.maximum(nsum, 1, out=nsum)   # avoid dividing by zero later
+    g = g / nsum.T
 
     # loop through lattice sites and reassign particle directions
     for coord in zip(*coords):
@@ -68,18 +68,86 @@ def di_alignment(lgca):
         # calculate transition probabilities for directions
         weights = np.exp(lgca.beta * np.einsum('i,ij', g[coord], lgca.c))
         z = weights.sum()
-        weights = (weights / z)
-        #print("Coord:")
-        #print(coord)
-        #print("Weights:")
-        #print(weights)
+        # avoid division by zero if weights is zero
+        aux = np.nan_to_num(z)
+        weights = np.nan_to_num(weights)
+        weights = (weights / aux)
+        # In case there are rounding problems
+        if weights.sum() > 1:
+            weights = (weights / weights.sum())
 
         # reassign particle directions
-        #sample = npr.choice(lgca.c[0], size=(n,), replace=True, p=weights)
+        sample = npr.multinomial(n, weights, )
 
-        sample = npr.multinomial(lgca.c[0,0], weights, size = (n,) )
+        newnodes[coord] = sample
 
-        #print("Sample:")
-        #print(sample)
-        newnodes[coord] = np.array([np.count_nonzero(sample == 1), np.count_nonzero(sample == -1)])
     lgca.nodes = newnodes
+
+def go_or_grow(lgca):
+    """
+    interactions (switch, reorientation, birth, death) of the go-or-grow model for volume exclusion free LGCA
+    """
+    relevant = lgca.cell_density[lgca.nonborder] > 0
+    coords = [a[relevant] for a in lgca.nonborder]
+    n_m = lgca.nodes[..., :lgca.velocitychannels].sum(-1)
+    n_r = lgca.nodes[..., lgca.velocitychannels:].sum(-1)
+
+    for coord in zip(*coords):
+        # determine cell number and moving and resting cell population at coordinate
+        n = lgca.cell_density[coord]
+        n_mxy = n_m[coord]
+        n_rxy = n_r[coord]
+        rho = n / lgca.capacity
+
+        # phenotypic switch
+        j_1 = npr.binomial(n_mxy, tanh_switch(rho, kappa=lgca.kappa, theta=lgca.theta))
+        j_2 = npr.binomial(n_rxy, 1 - tanh_switch(rho, kappa=lgca.kappa, theta=lgca.theta))
+        n_mxy += j_2 - j_1
+        n_rxy += j_1 - j_2
+
+        # death
+        n_mxy -= npr.binomial(n_mxy * np.heaviside(n_mxy, 0), lgca.r_d)
+        n_rxy -= npr.binomial(n_rxy * np.heaviside(n_rxy, 0), lgca.r_d)
+
+        # birth
+        n_rxy += npr.binomial(n_rxy * np.heaviside(n_rxy, 0), np.maximum(lgca.r_b*(1-rho), 0))
+
+        # reorientation
+        v_channels = npr.multinomial(n_mxy, [1/lgca.velocitychannels]*lgca.velocitychannels)
+
+        # add resting cells and assign new content of node at the end of interaction step
+        r_channels = np.array([n_rxy])
+        node = np.hstack((v_channels, r_channels))
+        lgca.nodes[coord] = node
+
+
+def go_or_rest(lgca):
+    """
+    interactions (switch, reorientation) of the go-or-rest model for volume exclusion free LGCA
+    go-or-rest is a go-or-grow model without birth and death
+    """
+    relevant = lgca.cell_density[lgca.nonborder] > 0
+    coords = [a[relevant] for a in lgca.nonborder]
+    n_m = lgca.nodes[..., :lgca.velocitychannels].sum(-1)
+    n_r = lgca.nodes[..., lgca.velocitychannels:].sum(-1)
+
+    for coord in zip(*coords):
+        # determine cell number and moving and resting cell population at coordinate
+        n = lgca.cell_density[coord]
+        n_mxy = n_m[coord]
+        n_rxy = n_r[coord]
+        rho = n / lgca.capacity
+
+        # phenotypic switch
+        j_1 = npr.binomial(n_mxy, tanh_switch(rho, kappa=lgca.kappa, theta=lgca.theta))
+        j_2 = npr.binomial(n_rxy, 1 - tanh_switch(rho, kappa=lgca.kappa, theta=lgca.theta))
+        n_mxy += j_2 - j_1
+        n_rxy += j_1 - j_2
+
+        # reorientation
+        v_channels = npr.multinomial(n_mxy, [1/lgca.velocitychannels]*lgca.velocitychannels)
+
+        # add resting cells and assign new content of node at the end of interaction step
+        r_channels = np.array([n_rxy])
+        node = np.hstack((v_channels, r_channels))
+        lgca.nodes[coord] = node