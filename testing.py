d71130aa823170c66e6eb62d3909507b456b1449
sisyga91 <s1m0nsyga>
2022-07-12 13:31:57 +0200

    continued go-and-grow notebook further - wave speed analysis

--- testing.py
+++ testing.py
@@ -15,19 +15,23 @@ kappa = 0.
 theta = 0.5
 
 # simulation parameters
-dens = 0.2 #starting condition
+dens = 0.0001 #starting condition
 time = 100
 nodes = np.zeros(dims+(6+restchannels,), dtype=int)
-nodes[l//2, l//2, :] = 8
+nodes[l//2, l//2, 0] = 1
+nodes[l//4, l//4, 0] = 1
+nodes[l//4 * 3, l//4 * 3, 0] = 1
 
 
-lgca = get_lgca(interaction='go_or_grow', bc='periodic', density=dens, geometry=geom, dims=dims,
-                restchannels=restchannels, ve=False, ib=True,
-                r_d=r_d, r_b=r_b, kappa=kappa, theta=theta, nodes=nodes)
+
+lgca = get_lgca(interaction='random_walk', bc='periodic', density=dens, geometry=geom, dims=dims,
+                restchannels=restchannels, ve=True, ib=True, nodes=nodes,
+                r_d=r_d, r_b=r_b, kappa=kappa, theta=theta)
 lgca.timeevo(200, record=True)
 # lgca.plot_config()
-lgca.plot_prop_spatial()
+# lgca.plot_prop_spatial()
 # ani = lgca.live_animate_config(interval=500, grid=True)
 # ani = lgca.animate_flux()
+ani = lgca.animate_density()
 plt.show()
 
