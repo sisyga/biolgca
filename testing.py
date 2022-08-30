b9c91499593efedc15e99ea1483c6ccaf39d458a
simon.syga@web.de <simon.syga@web.de>
2022-08-30 11:43:35 +0200

    improved analytical prediction by using cut-off for proliferation

--- testing.py
+++ testing.py
@@ -6,7 +6,7 @@ from matplotlib import pyplot as plt
 # geometry
 geom = 'hx'
 restchannels = 6
-l = 20
+l = 50
 dims = l, l
 # model parameters
 
@@ -16,7 +16,7 @@ kappa = 0.
 theta = 0.5
 
 # simulation parameters
-dens = 0.01 #starting condition
+dens = 0.1 #starting condition
 beta = 1.2
 # time = 100
 # nodes = np.zeros(dims+(6+restchannels,), dtype=int)
@@ -29,12 +29,12 @@ beta = 1.2
 lgca = get_lgca(interaction='alignment', bc='periodic', density=dens, geometry=geom, dims=dims,
                 restchannels=restchannels, ve=1, ib=0, beta=beta,
                 r_d=r_d, r_b=r_b, kappa=kappa, theta=theta)
-lgca.timeevo(50, record=1)
+# lgca.timeevo(50, record=1)
 # lgca.plot_config()
 # lgca.plot_prop_spatial()
-lgca.plot_config(grid=1)
+# lgca.plot_config(grid=1)
 # ani = lgca.animate_config(interval=500, grid=1)
-# ani = lgca.animate_flux()
+ani = lgca.live_animate_flux()
 # ani = lgca.animate_density()
 # lgca.plot_flux(cbar=0)
 # plt.gca().axis('off')
