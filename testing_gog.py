eaff5e576c4581819d9cfb4790d46285b792158a
simon.syga@web.de <simon.syga@web.de>
2023-01-10 16:51:22 +0100

    prototype working, do more simulations and figures now!

--- testing_gog.py
+++ testing_gog.py
@@ -13,18 +13,18 @@ r_d = 0.2 * r_b / 2 # initial death rate
 
 # nodes = np.zeros(dims+(6+restchannels,), dtype=int)
 # nodes[l//2, l//2, -1] = capacity
-
+#
 nodes = np.zeros((l,)+(2+restchannels,), dtype=int)
 nodes[l//2, -1] = capacity
 kappa = np.random.random(capacity) * 8 - 4
 
 # lgca = get_lgca(ib=True, bc='reflect', interaction='go_or_grow', dims=dims, nodes=nodes, ve=False, geometry='hx',
-#                 r_b=r_b, capacity=capacity, r_d=r_d, kappa=kappa)
+#                 r_b=r_b, capacity=capacity, r_d=r_d, kappa=kappa, theta_std=1e-6)
 lgca = get_lgca(ib=True, bc='reflect', interaction='go_or_grow', dims=l, nodes=nodes, ve=False, geometry='lin',
                 r_b=r_b, capacity=capacity, r_d=r_d, kappa=kappa, theta_std=1e-6)
 
 
-lgca.timeevo(700, record=True, recordN=True)
+lgca.timeevo(1000, record=True, recordN=False)
 
 kappas = lgca.get_prop(propname='kappa')
 # anim = lgca.animate_density()
