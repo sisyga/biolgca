d4b565b1f15f42008a72bfbebd7800f9f3300953
simon.syga@web.de <simon.syga@web.de>
2023-03-24 10:43:13 +0100

    unimportant tests

--- testing_gog.py
+++ testing_gog.py
@@ -4,28 +4,28 @@ from lgca import get_lgca
 from lgca.nove_ib_interactions import go_or_grow_kappa_chemo
 
 restchannels = 1
-l = 1000
+l = 100
 dims = l, l
-capacity = 100
+capacity = 50
 # interaction parameters
 r_b = 1. # initial birth rate
-r_d = 0.5 * r_b / 2 # initial death rate
+r_d = 0.4 * r_b / 2 # initial death rate
 # r_b = 0 # initial birth rate
 # r_d = 0.# initial death rate
-# nodes = np.zeros(dims+(6+restchannels,), dtype=int)
-# nodes[l//2, l//2, -1] = capacity
-
-nodes = np.zeros((l,)+(2+restchannels,), dtype=int)
-nodes[l//2, -1] = capacity
+nodes = np.zeros(dims+(6+restchannels,), dtype=int)
+nodes[l//2, l//2, -1] = capacity
+#
+# nodes = np.zeros((l,)+(2+restchannels,), dtype=int)
+# nodes[l//2, -1] = capacity
 kappa = np.random.random(capacity) * 8 - 4
 
-# rhoeq = 1 - r_d / r_b
-# lgca = get_lgca(ib=True, bc='reflect', interaction='go_or_grow', dims=dims, nodes=nodes, ve=False, geometry='hx',
-#                 r_b=r_b, capacity=capacity, r_d=r_d, kappa=kappa, theta_std=1e-6)
-lgca = get_lgca(ib=True, bc='reflect', interaction='go_or_grow_kappa', dims=l, nodes=nodes, ve=False, geometry='lin',
-                r_b=r_b, capacity=capacity, r_d=r_d, kappa=kappa, theta=.5, kappa_std=.2)
+rhoeq = 1 - r_d / r_b
+lgca = get_lgca(ib=True, bc='reflect', interaction='go_or_grow_kappa', dims=dims, nodes=nodes, ve=False, geometry='hx',
+                r_b=r_b, capacity=capacity, r_d=r_d, kappa=kappa, theta=rhoeq/2)
+# lgca = get_lgca(ib=True, bc='reflect', interaction='go_or_grow_kappa', dims=l, nodes=nodes, ve=False, geometry='lin',
+#                 r_b=r_b, capacity=capacity, r_d=r_d, kappa=kappa, kappa_std=.2)
 
-lgca.timeevo(500, record=True, recordN=False)
+lgca.timeevo(200, record=True, recordN=False)
 
 kappas = lgca.get_prop(propname='kappa')
 # anim = lgca.animate_density()
