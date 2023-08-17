a85e22a813be75fd42fc0ebfb1a0a5e7160d73fa
Simon <simon.syga@web.de>
2023-08-17 18:11:40 +0200

    analyzed spatial ratchet simulations. started work on plots.

--- paramscan_spatialratchet.py
+++ paramscan_spatialratchet.py
@@ -6,7 +6,7 @@ from tqdm.auto import tqdm
 # cancer params
 
 r_b = 0.5
-r_d = r_b / 4 * 3
+r_d = r_b / 4 * 3.
 a_max = 1
 # p_mut = 1
 p_l = 1e-8
@@ -44,40 +44,42 @@ mucrit = s_d / T_p / r_b
 # gamma = np.log(t_half * N0)  # approximate gamma value
 # D = 1. / (2 + np.exp(gamma))
 # Tfill = l / (2 * np.sqrt(D * (r_b - r_d)))
-nK = 5
-ngamma = 5
-Ks = np.logspace(-1, .5, nK) * Ncrit  # adjust later, very broad range now
-Ks = Ks.astype(int)
-gammas = np.linspace(0, 18, ngamma)
-reps = 10
-p_cancer = np.zeros((len(gammas), len(Ks), reps))
-print('Ks: ', Ks, 'gammas: ', gammas, 'reps: ', reps, 'Ncrit: ', Ncrit)
+nK = 21
+ngamma = 21
+Ks = np.logspace(-1, 1, nK) * Ncrit  # adjust later, very broad range now
+Ks = np.round(Ks)
+# Ks = np.linspace(.1, Ncrit, 20, dtype=int)
+gammas = np.linspace(0, 20, ngamma)
+reps = 100
+p_cancer = np.zeros((len(gammas), len(Ks), reps), dtype=bool)
+print('Ks: ', Ks, 'gammas: ', gammas, 'reps: ', reps, 'Ncrit: ', Ncrit, 'N0: ', dens0 * Ks[-1], 'mucrit: ', mucrit,
+      'p_l: ', p_l)
 # use tqdm to get a progress bar for the three loops
 
 with tqdm(total=len(gammas) * len(Ks) * reps) as pbar:
     for i, gamma in enumerate(gammas):
         for j, K in enumerate(Ks):
             for rep in range(reps):
-                l = round(np.ceil(8 * Ncrit / K))
-                dims = l,
                 N0 = dens0 * K
+                l = round(np.ceil(4 * Ncrit / N0))
+                dims = l,
                 nodes = np.zeros((l, 3), dtype=int)
-                nodes[0, 0] = round(dens0 * K)
+                nodes[0, 0] = round(N0)
                 lgca = get_lgca(ib=True, nodes=nodes, bc='refl', interaction='birthdeath_cancerdfe', ve=False, capacity=K,
                                 r_d=r_d, r_b=r_b, a_max=a_max, geometry='lin', dims=dims, restchannels=1, s_d=s_d, p_d=p_d,
                                 p_p=p_p, s_p=s_p, gamma=gamma)
                 n = nodes.sum()
-                while 0 < n < round(3 * N0 / 4):
+                while 0 < n < max(2 * N0, Ncrit):
                     lgca.timestep()
                     n = lgca.cell_density[lgca.nonborder].sum()
 
                 if n > 0:
-                    p_cancer[i, j, rep] = 1
+                    p_cancer[i, j, rep] = True
 
                 pbar.update(1)
 
 
-np.save('p_cancer_trial1.npy', p_cancer)
-np.savez(Ks=Ks, gammas=gammas, reps=reps, Ncrit=Ncrit, r_b=r_b, r_d=r_d, p_l=p_l, s_d=s_d, p_d=p_d, p_p=p_p, s_p=s_p,
+np.save('p_cancer_full.npy', p_cancer)
+np.savez('p_cancer_fulltrial_parameters.npz', Ks=Ks, gammas=gammas, reps=reps, Ncrit=Ncrit, r_b=r_b, r_d=r_d, p_l=p_l, s_d=s_d, p_d=p_d, p_p=p_p, s_p=s_p,
          T_d=T_d, T_p=T_p, p_mut=p_mut, dens0=dens0, )
 
