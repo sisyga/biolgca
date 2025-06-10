8bdb4c0408bf5d32603c38cf05dee616019e5d2a
simon.syga@web.de <simon.syga@web.de>
2025-06-10 11:06:46 +0200

    testing git integration VSCode

--- testing.py
+++ testing.py
@@ -1,7 +1,8 @@
 from lgca import get_lgca
+from matplotlib import pyplot as plt
 
+lgca = get_lgca(dims=(10, 10), bc='refl', ib=False, ve=False, geometry='square', interaction='go_or_grow', capacity=200,
+                kappa=-1, restchannels=100)
+print(lgca)
 
-lgca_sq = get_lgca(geometry='square', dims=4)
-print(lgca_sq.c)
-
-
+#%%
