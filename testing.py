bd534a03730955bdc18ee9bef397273e7c995e7e
simon.syga@web.de <simon.syga@web.de>
2025-06-12 12:02:11 +0200

    pulled refactor, issue with 3dmoore calc permutations (not found)

--- testing.py
+++ testing.py
@@ -1,8 +1,10 @@
 from lgca import get_lgca
 from matplotlib import pyplot as plt
 
-lgca = get_lgca(dims=(10, 10), bc='refl', ib=False, ve=False, geometry='square', interaction='go_or_grow', capacity=200,
-                kappa=-1, restchannels=100)
+lgca = get_lgca(dims=(20, 20, 20), bc='refl', ib=False, ve=True, geometry='moore', interaction='alignment',
+                beta=1, restchannels=0)
 print(lgca)
+lgca.timeevo(100, record=True)
+lgca.animate_density()
 
 #%%
