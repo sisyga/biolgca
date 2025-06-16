d2fe759e504d76859001d87c9ae397431a2f4ca1
simon.syga@web.de <simon.syga@web.de>
2025-06-16 12:23:54 +0200

    rolled back buggy interaction functions in interactions.py

--- testing.py
+++ testing.py
@@ -2,8 +2,8 @@ from matplotlib import pyplot as plt
 
 from lgca import get_lgca
 
-lgca = get_lgca(dims=(100, 100), bc='refl', ib=False, ve=True, geometry='hex', interaction='alignment',
-                beta=3, restchannels=0)
+lgca = get_lgca(dims=100, bc='refl', ib=False, ve=True, geometry='hex', interaction='nematic',
+                beta=4, restchannels=1)
 print(lgca)
 lgca.timeevo(100, record=True)
 anim = lgca.animate_flux()
