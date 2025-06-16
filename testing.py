228dfc6809aeef0c8b720c4bc96550929f620cea
simon.syga@web.de <simon.syga@web.de>
2025-06-16 17:54:00 +0200

    removed nove_ib_test.py content for now, was too buggy, needs to be reimplemented from scratch

--- testing.py
+++ testing.py
@@ -1,12 +1,14 @@
-from matplotlib import pyplot as plt
-
+# from matplotlib import pyplot as plt
+from mayavi import mlab
 from lgca import get_lgca
 
-lgca = get_lgca(dims=100, bc='refl', ib=False, ve=True, geometry='hex', interaction='nematic',
-                beta=4, restchannels=1)
+
+lgca = get_lgca(dims=10, bc='refl', ib=False, ve=False, geometry='moore', interaction='di_alignment', density=0.02,
+                beta=3, restchannels=0, nb_include_center=False)
 print(lgca)
-lgca.timeevo(100, record=True)
+lgca.timeevo(10, record=True)
 anim = lgca.animate_flux()
-plt.show()
+# plt.show()
+mlab.show()
 
 #%%
