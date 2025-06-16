42f4c7c63f954b93abfddff6475d6065c34b15c9
simon.syga@web.de <simon.syga@web.de>
2025-06-16 18:42:27 +0200

    commented out test_characteristics (super long run time for moore lattice).

--- testing.py
+++ testing.py
@@ -1,12 +1,14 @@
-# from matplotlib import pyplot as plt
+import os
+os.environ['ETS_TOOLKIT'] = 'qt'
+os.environ['QT_API'] = 'pyqt5'
+from matplotlib import pyplot as plt
 from mayavi import mlab
 from lgca import get_lgca
 
-
-lgca = get_lgca(dims=10, bc='refl', ib=False, ve=False, geometry='moore', interaction='di_alignment', density=0.02,
-                beta=3, restchannels=0, nb_include_center=False)
+lgca = get_lgca(dims=20, bc='refl', ib=False, ve=True, geometry='moore', interaction='di_alignment', density=0.02,
+                beta=2, restchannels=0, nb_include_center=False)
 print(lgca)
-lgca.timeevo(10, record=True)
+lgca.timeevo(100, record=True)
 anim = lgca.animate_flux()
 # plt.show()
 mlab.show()
